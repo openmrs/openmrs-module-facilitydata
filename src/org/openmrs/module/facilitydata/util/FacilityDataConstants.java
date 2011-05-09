065557cc4c853385d4dbda6906618199c1f82f64
mseaton <mseaton@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-05-09 17:29:46 +0000

    facilitydata: Add ability to hide certain locations (eg. Unknown Location) from the list of supported facilities

--- src/org/openmrs/module/facilitydata/util/FacilityDataConstants.java
+++ src/org/openmrs/module/facilitydata/util/FacilityDataConstants.java
@@ -1,5 +1,13 @@
 package org.openmrs.module.facilitydata.util;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+
 /**
  * Constants for use within the module
  */
@@ -10,4 +18,21 @@ public class FacilityDataConstants {
     public static final String ENTER = "Enter Facility Data Reports";
     public static final String VIEW = "View Facility Data Reports";
     public static final String MANAGE = "Manage Facility Data Reports";
+    
+    // GLOBAL PROPERTIES
+    
+    public static List<Location> getSupportedFacilities() {
+    	List<Location> ret = Context.getLocationService().getAllLocations();
+    	String limitToFacilitiesGp = Context.getAdministrationService().getGlobalProperty("facilitydata.unsupportedFacilities");
+    	if (StringUtils.isNotBlank(limitToFacilitiesGp)) {
+    		List<String> locIds = Arrays.asList(limitToFacilitiesGp.split(","));
+    		for (Iterator<Location> i = ret.iterator(); i.hasNext();) {
+    			Location l = i.next();
+    			if (locIds.contains(l.getLocationId().toString())) {
+    				i.remove();
+    			}
+    		}
+    	}
+    	return ret;
+    }
 }
